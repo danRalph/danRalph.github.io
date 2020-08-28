a7d656c393417ec74499bba94bab746d73ec5d94
danRalph <dan.r.richards@hotmail.co.uk>
2020-08-28 14:59:16 +0100

    Updates

---
+++ precache-manifest.802bcc7e5195460cca76602c7dea33da.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7eeceef52a011225cdeea72982fac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4142b98ac586471c391c",
+    "url": "/static/css/main.201fb916.chunk.css"
+  },
+  {
+    "revision": "8fc11fd21d5eea4dab03",
+    "url": "/static/js/2.0c8d6a0f.chunk.js"
+  },
+  {
+    "revision": "37dd8081c80c4ed815bd181dcc60f176",
+    "url": "/static/js/2.0c8d6a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4142b98ac586471c391c",
+    "url": "/static/js/main.d7f2630a.chunk.js"
+  },
+  {
+    "revision": "65d5baf968f6ef87cccc",
+    "url": "/static/js/runtime-main.9debeb51.js"
+  },
+  {
+    "revision": "15661a016322817b82be076fe839cd4c",
+    "url": "/static/media/css3.15661a01.png"
+  },
+  {
+    "revision": "b3a3c3973ade1039319ac3208878578d",
+    "url": "/static/media/gh2.b3a3c397.png"
+  },
+  {
+    "revision": "a9131c01671bfc5c7119d341ee113a99",
+    "url": "/static/media/jq2.a9131c01.png"
+  },
+  {
+    "revision": "2d146e5fc28c161ad2c13fd6057d83d3",
+    "url": "/static/media/pro1.2d146e5f.png"
+  },
+  {
+    "revision": "3af99868b0b0edda0219d9f282d71eab",
+    "url": "/static/media/pro2.3af99868.png"
+  },
+  {
+    "revision": "fac0ea9cc14b36d69d6ddd5e7941216f",
+    "url": "/static/media/pro3.fac0ea9c.png"
+  },
+  {
+    "revision": "3836d23ecd403b5fd3a02263c8bb0b78",
+    "url": "/static/media/pro4.3836d23e.png"
+  },
+  {
+    "revision": "1bf10e6713556a89a8bbe186fb307b2c",
+    "url": "/static/media/pro5.1bf10e67.png"
+  },
+  {
+    "revision": "f3d1625e0ea42809b52b0e3d33df3a02",
+    "url": "/static/media/ps2.f3d1625e.png"
+  },
+  {
+    "revision": "0efe3d9c57b60d0b7d1bc303d6a9bcc2",
+    "url": "/static/media/react2.0efe3d9c.png"
+  },
+  {
+    "revision": "eb2f0fd8ab31f310c1dd05bf7b5bd2c5",
+    "url": "/static/media/wol3.eb2f0fd8.jpg"
+  },
+  {
+    "revision": "9ba3d89469a19224e4406f4f7ed4fe68",
+    "url": "/static/media/wolff.9ba3d894.jpg"
+  }
+]);
